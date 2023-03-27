4f9376baed99e99aac50c6c2fd0e0488f9b10386
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-03-27 16:23:26 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,68 +1,73 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { Buttons } from './Buttons/Buttons';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 import data from './data.json';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => {
+    const total = good + neutral + bad;
+    return total;
   };
 
-  countTotalFeedback() {
-    const values = Object.values(this.state);
-    const total = values.reduce((x, y) => x + y);
-    return total;
-  }
-
-  countPositiveFeedbackPercentage() {
-    const total = this.countTotalFeedback();
-    const percentage = (this.state.good / total) * 100;
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    const percentage = (good / total) * 100;
     if (percentage > 0 && percentage !== Infinity)
       return Math.round(percentage) + '%';
-  }
-
-  handleButtonClick = e => {
-    const feedback = e.target.name;
-    this.setState(prevState => ({ [feedback]: prevState[feedback] + 1 }));
   };
 
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-          textAlign: 'center',
-          flexDirection: 'column',
-        }}
-      >
-        <Section title={data.heading}>
-          <Buttons
-            content={data.btnText}
-            clickHandler={this.handleButtonClick}
+  const handleButtonClick = e => {
+    const { name } = e.target;
+
+    switch (name) {
+      case 'good':
+        setGood(s => s + 1);
+        break;
+      case 'neutral':
+        setNeutral(s => s + 1);
+        break;
+      case 'bad':
+        setBad(s => s + 1);
+        break;
+      default:
+        return;
+    }
+  };
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        textAlign: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <Section title={data.heading}>
+        <Buttons content={data.btnText} clickHandler={handleButtonClick} />
+      </Section>
+      <Section title={data.statisticsTitle}>
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title={data.statisticsTitle}>
-          {this.countTotalFeedback() > 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message={data.defaultText} />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        ) : (
+          <Notification message={data.defaultText} />
+        )}
+      </Section>
+    </div>
+  );
 }
